90be73a4408de3756ddfe5c4efad2823f6b86529
s-wlii <marshmello.sw@gmail.com>
2022-01-05 01:26:27 +0900

    add files
    add react, lec20 fin

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("./config/key");
-const { auth } = require("./middleware/auth");
+const config = require("./server/config/key");
+const { auth } = require("./server/middleware/auth");
 
-const { User } = require("./models/User");
+const { User } = require("./server/models/User");
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:0376@nodereact.jqwxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
