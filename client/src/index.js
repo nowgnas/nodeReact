0b56f06695b345723a9a4f9a66169876aba63fff
s-wlii <marshmello.sw@gmail.com>
2022-01-06 01:22:55 +0900

    add login page
    add handler

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 // import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      // redux extention추가 하기
+      window.__REDUX_DEVTOOLS_EXTENTION__ &&
+        window.__REDUX_DEVTOOLS_EXTENTION__()
+    )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
