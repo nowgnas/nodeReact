802669532f6dd1c8c13681243a9a9818ba75643f
s-wlii <marshmello.sw@gmail.com>
2022-01-06 00:11:58 +0900

    del dev.js

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://admin:0376@nodereact-shard-00-00.jqwxu.mongodb.net:27017,nodereact-shard-00-01.jqwxu.mongodb.net:27017,nodereact-shard-00-02.jqwxu.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-osh24y-shard-0&authSource=admin&retryWrites=true&w=majority",
-};
